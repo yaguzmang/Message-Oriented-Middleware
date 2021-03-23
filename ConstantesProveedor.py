7c3355fe99225597e403c9447a53c3961b44b257
Yhoan Alejandro Guzman Garcia <yaguzmang@eafit.edu.co>
2021-03-22 20:51:53 -0500

    colar terminado

--- ConstantesProveedor.py
+++ ConstantesProveedor.py
@@ -4,7 +4,7 @@ Created on Created on Sun Mar 21 2021
 @author: Daniel Felipe Gomez Martinez, Juan Sebastian Perez Salazar and Yhoan Alejandro Guzman Garcia
 """
 
-constantes_proveedor = ["REGISTRAR","SALIR","CONECTAR","CREAR_CANAL","LISTAR_CANAL","BORRAR_CANAL","ENVIAR_MENSAJE_CANAL", "CREAR_TAREA", "LISTAR_TAREAS"]
+constantes_proveedor = ["REGISTRAR","SALIR","CONECTAR","CREAR_CANAL","LISTAR_CANAL","BORRAR_CANAL","ENVIAR_MENSAJE_CANAL", "CREAR_TAREA", "LISTAR_TAREAS", "LISTAR_TAREAS_R"]
 
 salir = "SALIR"
 registrar = "REGISTRAR"
@@ -15,3 +15,4 @@ listar_canal = "LISTAR_CANAL"
 borrar_canal = "BORRAR_CANAL"
 enviar_mensaje = "ENVIAR_MENSAJE_CANAL"
 listar_tareas = "LISTAR_TAREAS"
+listar_tareas_r = "LISTAR_TAREAS_R"
--- Server.py
+++ Server.py
@@ -7,138 +7,54 @@ import socket
 import Constantes
 import ConstantesConsumidor
 import time
+import random
 
-class App:
+class Server:
     def __init__(self):
-        self.socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.token = "tokenejemplo1"
 
     def run_app(self):
-        while True:
-            print('Hola Mundo!')
-            time.sleep(10)
         print('*' * 50)
         print("SERVER RUNNING\n")
-        socket_consumidor.connect((Constantes.direccion_conexion_consumidor, Constantes.puerto))
-        tupla_conexion = socket_consumidor.getsockname()
-        print("SERVER CONNECTED: ", tupla_conexion)
-
+        tarea_fue_realizada = False
         while True:
-            if opcion == '':
-                print("Opcion invalida, intenta de nuevo\n")
-            elif (opcion == ConstantesConsumidor.conectar_consumidor):
-                nombre_canal = input("Ingresa el nombre del canal al que te quieres conectar ")
-                id_canal = input("Ingresa el id del canal al que te quieres conectar ")
-                envio_MOM = opcion + ' ' + nombre_canal + ' ' + id_canal + ' ' + token 
-                socket_consumidor.send(bytes(envio_MOM, Constantes.formato_decodificacion))
-                datos_recibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-                print(datos_recibidos.decode(Constantes.formato_decodificacion))
-            elif (opcion == ConstantesConsumidor.listar_canal):
-                envio_MOM = opcion
-                socket_consumidor.send(bytes(envio_MOM, Constantes.formato_decodificacion))
-                datos_recibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-                print(datos_recibidos.decode(Constantes.formato_decodificacion))
-                datos_recibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-                print(datos_recibidos.decode(Constantes.formato_decodificacion))
-            elif (opcion.split()[0] == ConstantesConsumidor.registrar):
-                envio_MOM = opcion
-                socket_consumidor.send(bytes(envio_MOM, Constantes.formato_decodificacion))
-                datosRecibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-                print(datosRecibidos.decode(Constantes.formato_decodificacion))
-
-            elif (opcion.split()[0] == ConstantesConsumidor.conectar):
-                envio_MOM = opcion
-                socket_consumidor.send(bytes(envio_MOM, Constantes.formato_decodificacion))
-                datosRecibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-                respuesta= datosRecibidos.decode(Constantes.formato_decodificacion)
-                if(respuesta[0:6] == "token="):
-                    token = respuesta[6:]
-                    print("Bienvenido "+ opcion.split()[1])
-                elif(respuesta == "Contraseña incorrecta"):
-                    print("Contraseña incorrecta para el proveedor " + opcion.split()[1])
-                else:
-                    print("No existe un proveedor que se llame " + opcion.split()[1])
-            elif (opcion == ConstantesConsumidor.recibir_mensaje):
-                if(token != ""):
-                    nombre_canal = input("Ingresa el nombre del canal del cual quieres recibir un mensaje ")
-                    id_canal = input("Ingresa el id del canal del cual quieres recibir un mensaje ")
-                    envio_MOM = opcion + ' ' + nombre_canal + ' ' + id_canal + ' ' + token
-                    socket_consumidor.send(bytes(envio_MOM, Constantes.formato_decodificacion))
-                    datosRecibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-                    print(datosRecibidos.decode(Constantes.formato_decodificacion))
+            if not tarea_fue_realizada:
+                self.socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.socket_server.connect((Constantes.direccion_conexion_consumidor, Constantes.puerto))
+                tupla_conexion = self.socket_server.getsockname()
+            print("SERVER CONNECTED: ", tupla_conexion)
+            envio_MOM = ConstantesConsumidor.asignar_tarea
+            self.socket_server.send(bytes(envio_MOM, Constantes.formato_decodificacion))
+            datos_recibidos = self.socket_server.recv(Constantes.tamaño_buffer)
+            print(datos_recibidos.decode(Constantes.formato_decodificacion))
+            datos_recibidos = self.socket_server.recv(Constantes.tamaño_buffer)
+            print('Mensaje recibido')
+            respuesta = datos_recibidos.decode(Constantes.formato_decodificacion)
+            if respuesta.lower().strip() == "No hay tareas en el MOM".lower().strip():
+               tarea_fue_realizada = False
+               print(respuesta)
+               time.sleep(5)
             else:
-                print("Opción invalida, intenta de nuevo\n")
-
-        socket_consumidor.send(bytes(opcion, Constantes.formato_decodificacion))
-        datos_recibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-        print(datos_recibidos.decode(Constantes.formato_decodificacion))
-        socket_consumidor.close()
-
-
-socket_consumidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-token = ""
-
+                print("Tarea a realizar: " + respuesta)
+                info_tarea = respuesta.split(':')
+                self.socket_server.close()
+                tiempo_ex = random.randint(5, 10)
+                time.sleep(tiempo_ex)
+                tarea_fue_realizada = True
+                self.socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.socket_server.connect((Constantes.direccion_conexion_consumidor, Constantes.puerto))
+                tupla_conexion = self.socket_server.getsockname()
+                envio_MOM = ConstantesConsumidor.tarea_realizada + ' ' +  info_tarea[0] + ' ' + info_tarea[1] + ' ' + str(tiempo_ex)
+                self.socket_server.send(bytes(envio_MOM, Constantes.formato_decodificacion))
+                datos_recibidos = self.socket_server.recv(Constantes.tamaño_buffer)
+                respuesta = datos_recibidos.decode(Constantes.formato_decodificacion)
+                print(respuesta)
 def main():
-    print('*' * 50)
-    print("SERVER RUNNING\n")
-    socket_consumidor.connect((Constantes.direccion_conexion_consumidor, Constantes.puerto))
-    tupla_conexion = socket_consumidor.getsockname()
-    print("SERVER CONNECTED: ", tupla_conexion)
-
-    while True:
-        if opcion == '':
-            print("Opcion invalida, intenta de nuevo\n")
-        elif (opcion == ConstantesConsumidor.conectar_consumidor):
-            nombre_canal = input("Ingresa el nombre del canal al que te quieres conectar ")
-            id_canal = input("Ingresa el id del canal al que te quieres conectar ")
-            envio_MOM = opcion + ' ' + nombre_canal + ' ' + id_canal + ' ' + token 
-            socket_consumidor.send(bytes(envio_MOM, Constantes.formato_decodificacion))
-            datos_recibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-            print(datos_recibidos.decode(Constantes.formato_decodificacion))
-        elif (opcion == ConstantesConsumidor.listar_canal):
-            envio_MOM = opcion
-            socket_consumidor.send(bytes(envio_MOM, Constantes.formato_decodificacion))
-            datos_recibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-            print(datos_recibidos.decode(Constantes.formato_decodificacion))
-            datos_recibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-            print(datos_recibidos.decode(Constantes.formato_decodificacion))
-        elif (opcion.split()[0] == ConstantesConsumidor.registrar):
-            envio_MOM = opcion
-            socket_consumidor.send(bytes(envio_MOM, Constantes.formato_decodificacion))
-            datosRecibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-            print(datosRecibidos.decode(Constantes.formato_decodificacion))
-
-        elif (opcion.split()[0] == ConstantesConsumidor.conectar):
-            envio_MOM = opcion
-            socket_consumidor.send(bytes(envio_MOM, Constantes.formato_decodificacion))
-            datosRecibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-            respuesta= datosRecibidos.decode(Constantes.formato_decodificacion)
-            if(respuesta[0:6] == "token="):
-                token = respuesta[6:]
-                print("Bienvenido "+ opcion.split()[1])
-            elif(respuesta == "Contraseña incorrecta"):
-                print("Contraseña incorrecta para el proveedor " + opcion.split()[1])
-            else:
-                print("No existe un proveedor que se llame " + opcion.split()[1])
-        elif (opcion == ConstantesConsumidor.recibir_mensaje):
-            if(token != ""):
-                nombre_canal = input("Ingresa el nombre del canal del cual quieres recibir un mensaje ")
-                id_canal = input("Ingresa el id del canal del cual quieres recibir un mensaje ")
-                envio_MOM = opcion + ' ' + nombre_canal + ' ' + id_canal + ' ' + token
-                socket_consumidor.send(bytes(envio_MOM, Constantes.formato_decodificacion))
-                datosRecibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-                print(datosRecibidos.decode(Constantes.formato_decodificacion))
-        else:
-            print("Opción invalida, intenta de nuevo\n")
-
-    socket_consumidor.send(bytes(opcion, Constantes.formato_decodificacion))
-    datos_recibidos = socket_consumidor.recv(Constantes.tamaño_buffer)
-    print(datos_recibidos.decode(Constantes.formato_decodificacion))
-    socket_consumidor.close()
-
+    server()
+    
 def server():
 	server = Server()
-	server.main()
+	server.run_app()
 
 if __name__ == '__main__':
 	server()
