0990205eeeddf3752e31e300fe0a2a09085c370b
Daniel Gomez <dfgomezmstarter>
2021-03-22 23:44:24 -0500

    constantes para añadir color a los prints

--- Constantes.py
+++ Constantes.py
@@ -3,6 +3,7 @@ Created on Created on Sun Mar 21 2021
 
 @author: Daniel Felipe Gomez Martinez, Juan Sebastian Perez Salazar and Yhoan Alejandro Guzman Garcia
 """
+from colorama import init, Fore, Back, Style
 
 direccion_conexion_servidor = "127.0.0.1" # 0.0.0.0
 direccion_conexion_consumidor = "127.0.0.1" # 54.83.133.119
@@ -11,3 +12,7 @@ puerto = 1234 #8080
 reserva = 5
 tamaño_buffer = 1024
 formato_decodificacion = "utf-8"
+comandos = Fore.CYAN + Style.BRIGHT
+error = Fore.RED + Style.BRIGHT
+exito = Fore.GREEN + Style.BRIGHT
+entradas = Fore.YELLOW + Style.BRIGHT
